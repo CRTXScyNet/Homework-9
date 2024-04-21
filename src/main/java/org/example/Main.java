e466920c49fc4a20f8bdde7287c71f35daab1783
CRTXScyNet <CRTXScyNet@gmail.com>
2024-04-21 19:55:50 +0500

    HW-9 to check

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,8 @@ public class Main {
         // Task one
         int[] money = {151,23,346,34,112};
         int total = 0;
-        for (int i = 0; i < money.length; i++) {
-            total += money[i];
+        for (int j : money) {
+            total += j;
         }
         System.out.println("Сумма трат за месяц составила " + total + " рублей");
 
@@ -18,12 +18,12 @@ public class Main {
         int[] money2 = {151,23,346,34,112};
         int min = -1;
         int max = -1;
-        for (int i = 0; i < money2.length; i++) {
-            if (max < money2[i]) {
-                max = money2[i];
+        for (int j : money2) {
+            if (max < j) {
+                max = j;
             }
-            if (min > money2[i] || min < 0) {
-                min = money2[i];
+            if (min > j || min < 0) {
+                min = j;
             }
         }
         System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей. Максимальная сумма трат за неделю составила " + max + " рублей");
@@ -31,8 +31,8 @@ public class Main {
         // Task three
         float[] money3 = {151,23,346,34,112};
         float total3 = 0;
-        for (int i = 0; i < money3.length; i++) {
-            total3 += money3[i];
+        for (float v : money3) {
+            total3 += v;
         }
         float averageExpense = total3/ money3.length;
         System.out.println("Средняя сумма трат за месяц составила " + averageExpense + " рублей");
